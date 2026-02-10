97a675cdfe3fc610e0201c1be422e7ea8017b5cd
mghayask25 <claiffben@gmail.com>
2026-02-10 23:08:35 +0500

    Project Completed

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let isStatus = document.querySelector("h5");
-let addFriend = document.querySelector("#addFriend");
+let btn = document.querySelector("#addFriend");
 let check = 0;
-addFriend.addEventListener("click" , function(){
+btn.addEventListener("click" , function(){
     if(check === 0){
         isStatus.innerHTML = "Friends";
         isStatus.style.color = "Green";
+        btn.innerHTML = "Remove Friend"
         check = 1;
     }
     else{
         isStatus.innerHTML = "Stranger";
         isStatus.style.color = "Red";
+        btn.innerHTML = "Add Friend"
         check = 0;
     }
 });
